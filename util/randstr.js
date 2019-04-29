8e75b450259b81011d01396b0bd6f488b6613ffe
Kazuichi Takashiro <takashiro@qq.com>
2019-04-29 21:28:33 +0800

    Enforce spaces before/after keywords

---
+++ util/randstr.js
@@ -0,0 +1,21 @@
+
+function randstr(length) {
+	let str = '';
+	for (let i = 0; i < length; i++){
+		let rand = Math.floor(Math.random() * 62);
+		if (rand >= 0 && rand <= 25){
+			rand += 0x41;
+		} else if (rand <= 51){
+			rand -= 26;
+			rand += 0x61;
+		} else {
+			rand -= 52;
+			rand += 0x30;
+		}
+		str += String.fromCharCode(rand);
+	}
+
+	return str;
+}
+
+module.exports = randstr;
---
+++ util/shuffle.js
@@ -0,0 +1,11 @@
+
+function shuffle(a) {
+	for (let i = a.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let x = a[i];
+		a[i] = a[j];
+		a[j] = x;
+	}
+}
+
+module.exports = shuffle;
