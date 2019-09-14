1cd1954edbb03be59209dde5d534ee7f6d8c1f5c
Kazuichi Takashiro <takashiro@qq.com>
2019-09-14 21:29:22 +0800

    Initial hegemony rule

---
+++ mode/hegemony/hegemony-rules.js
@@ -0,0 +1,17 @@
+
+const GameRule = require('../driver/GameRule');
+const cmd = require('../cmd');
+
+class StartGame extends GameRule {
+
+	async effect(driver, player, data) {
+		await this.arrangeGenerals();
+		this.room.broadcast(cmd.ToBattle);
+		this.arrangeCards();
+	}
+
+}
+
+module.exports = [
+	StartGame,
+];
