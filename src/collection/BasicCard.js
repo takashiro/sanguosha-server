96a168defabb78a0915546359a96f23de9df5b3f
Kazuichi Takashiro <takashiro@qq.com>
2019-11-10 23:31:05 +0800

    Basic cards should goes via process area

--- src/collection/BasicCard.js
+++ src/collection/BasicCard.js
@@ -2,6 +2,27 @@
 const Card = require('../core/Card');
 
 class BasicCard extends Card {
+	async use(driver, use) {
+		const { card } = use;
+		if (!card.isReal()) {
+			return;
+		}
+
+		const handArea = use.from.getHandArea();
+		const processArea = use.from.getProcessArea();
+		driver.moveCards([card], handArea, processArea, { open: true });
+	}
+
+	async complete(driver, use) {
+		const { card } = use;
+		if (!card.isReal()) {
+			return;
+		}
+
+		const processArea = use.from.getProcessArea();
+		const discardPile = driver.getDiscardPile();
+		driver.moveCards([use.card], processArea, discardPile, { open: true });
+	}
 }
 
 module.exports = BasicCard;
