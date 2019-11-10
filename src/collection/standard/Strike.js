50ae43f52b8c4fb8c9a65d46fdf10b46359e4035
Kazuichi Takashiro <takashiro@qq.com>
2019-11-10 23:31:24 +0800

    Add a simple version of strike

--- src/collection/standard/Strike.js
+++ src/collection/standard/Strike.js
@@ -1,8 +1,46 @@
 const BasicCard = require('../BasicCard');
+const Phase = require('../../core/Player/Phase');
+
+const DamageStruct = require('../../driver/DamageStruct');
 
 class Strike extends BasicCard {
 	constructor(suit, number) {
 		super('strike', suit, number);
+	}
+
+	async isAvailable(source) {
+		return source && source.getPhase() === Phase.Play;
+	}
+
+	async targetFilter(selected, target, source) {
+		if (selected.length > 0 || !target) {
+			return false;
+		}
+
+		const driver = target.getDriver();
+		if (!driver) {
+			return false;
+		}
+
+		if (!source) {
+			return true;
+		}
+
+		if (source === target) {
+			return false;
+		}
+
+		const inRange = await driver.isInAttackRange(source, target);
+		return inRange;
+	}
+
+	async targetFeasible(selected) {
+		return selected.length === 1;
+	}
+
+	async effect(driver, effect) {
+		const damage = new DamageStruct(effect.from, effect.to, 1);
+		driver.damage(damage);
 	}
 }
 
