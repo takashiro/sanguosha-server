f314c2268ac632057c686f5fdb965589df1812f8
Kazuichi Takashiro <takashiro@qq.com>
2020-01-14 21:54:42 +0800

    Add CardUseMeta

--- src/driver/CardUseStruct.ts
+++ src/driver/CardUseStruct.ts
@@ -2,6 +2,17 @@ import { Card } from '@karuta/sanguosha-core';
 
 import ServerPlayer from './ServerPlayer';
 
+interface CardUseMeta {
+	from: number;
+	card: {
+		id: number;
+		name: string;
+		suit: number;
+		number: number;
+	};
+	to: number[] | undefined;
+}
+
 class CardUseStruct {
 	from: ServerPlayer;
 
@@ -13,6 +24,14 @@ class CardUseStruct {
 		this.from = from;
 		this.card = card;
 		this.to = to || [];
+	}
+
+	toJSON(): CardUseMeta {
+		return {
+			from: this.from.getSeat(),
+			card: this.card.toJSON(),
+			to: this.to.length > 0 ? this.to.map((player) => player.getSeat()) : undefined,
+		};
 	}
 }
 
