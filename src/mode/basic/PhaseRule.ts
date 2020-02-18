3d8e8032398bddc7447cb257e78ce5d0c2a9f695
Kazuichi Takashiro <takashiro@qq.com>
2020-02-18 21:07:31 +0800

    Rewrite PhaseRule

--- src/mode/basic/PhaseRule.ts
+++ src/mode/basic/PhaseRule.ts
@@ -1,64 +1,84 @@
+import { PlayerPhase as Phase } from '@karuta/sanguosha-core';
 
-const GameRule = require('../../driver/GameRule');
+import GameRule from '../../driver/GameRule';
+import GameEvent from '../../driver/GameEvent';
+import PhaseChangeStruct from '../../driver/PhaseChangeStruct';
+import DrawCardStruct from '../../driver/DrawCardStruct';
+import Card from '../../driver/Card';
+import ServerPlayer from '../../driver/ServerPlayer';
 
-const Phase = require('../../core/Player/Phase');
-const GameEvent = require('../../driver/GameEvent');
-
-class PhaseRule extends GameRule {
+class PhaseRule extends GameRule<PhaseChangeStruct> {
 	constructor() {
 		super(GameEvent.ProceedPhase);
 	}
 
-	isTriggerable(target) {
-		return Boolean(this.driver && target);
+	isTriggerable(data: PhaseChangeStruct): boolean {
+		return Boolean(this.driver && data && data.player);
 	}
 
-	async effect(player, data) {
+	async effect(data: PhaseChangeStruct): Promise<boolean> {
 		switch (data.to) {
 		case Phase.Draw:
-			await this.drawCards(player);
+			await this.drawCards(data.player);
 			break;
 		case Phase.Play:
-			await this.play(player);
+			await this.play(data.player);
 			break;
 		case Phase.Discard:
-			await this.discardCards(player);
+			await this.discardCards(data.player);
 			break;
 		default:
 			break;
 		}
+		return false;
 	}
 
-	async drawCards(player) {
-		const data = {
+	async drawCards(player: ServerPlayer): Promise<void> {
+		const data: DrawCardStruct = {
+			player,
 			num: 2,
 		};
 
-		const { driver } = this;
-		await driver.trigger(GameEvent.DrawNCards, player, data);
+		const driver = this.getDriver();
+		await driver.trigger(GameEvent.DrawNCards, data);
 		await driver.drawCards(player, data.num);
 	}
 
-	async play(player) {
+	async play(player: ServerPlayer): Promise<void> {
+		const driver = this.getDriver();
+		const handArea = player.getHandArea();
+		const cards = handArea.getCards();
+		const availableCards: Card[] = [];
+		for (const ccard of cards) {
+			const card = ccard as Card;
+			if (await card.isAvailable(driver, player)) {
+				availableCards.push(card);
+			}
+		}
 		for (;;) {
-			const valid = await player.play();
-			if (!valid) {
+			const action = await player.play(availableCards);
+			if (!action) {
 				break;
+			}
+
+			const card = action.card as Card;
+			if (card) {
+				driver.playCard(player, card);
 			}
 		}
 	}
 
-	async discardCards(player) {
-		const { handArea } = player;
+	async discardCards(player: ServerPlayer): Promise<void> {
+		const handArea = player.getHandArea();
 		const discardNum = handArea.size - player.getHp();
 		if (discardNum > 0) {
-			const selected = await player.askForCards(player.handArea, {
+			const selected = await player.askForCards(handArea, {
 				num: discardNum,
 			});
-			const { driver } = this;
-			driver.moveCards(selected, handArea, driver.discardPile, { open: true });
+			const driver = this.getDriver();
+			driver.moveCards(selected, handArea, driver.getDiscardPile(), { open: true });
 		}
 	}
 }
 
-module.exports = PhaseRule;
+export default PhaseRule;
