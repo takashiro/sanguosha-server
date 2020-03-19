8cbcb912814b545e162171b0639bfc87be8f2444
Kazuichi Takashiro <takashiro@qq.com>
2020-03-19 21:13:45 +0800

    Add basic procedure of trick cards

--- src/collection/TrickCard.ts
+++ src/collection/TrickCard.ts
@@ -1,10 +1,34 @@
 import { CardType as Type } from '@karuta/sanguosha-core';
 
 import Card from '../driver/Card';
+import GameDriver from '../driver/GameDriver';
+import CardUseStruct from '../driver/CardUseStruct';
 
 class TrickCard extends Card {
 	getType(): Type {
 		return Type.Trick;
+	}
+
+	async use(driver: GameDriver, use: CardUseStruct): Promise<void> {
+		const { card } = use;
+		if (!card.isReal()) {
+			return;
+		}
+
+		const handArea = use.from.getHandArea();
+		const processArea = use.from.getProcessArea();
+		driver.moveCards([card], handArea, processArea, { open: true });
+	}
+
+	async complete(driver: GameDriver, use: CardUseStruct): Promise<void> {
+		const { card } = use;
+		if (!card.isReal()) {
+			return;
+		}
+
+		const processArea = use.from.getProcessArea();
+		const discardPile = driver.getDiscardPile();
+		driver.moveCards([use.card], processArea, discardPile, { open: true });
 	}
 }
 
