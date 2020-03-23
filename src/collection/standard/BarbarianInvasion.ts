4fb0b5b290309d2045106eb8ced8233355ee6a81
Kazuichi Takashiro <takashiro@qq.com>
2020-03-23 22:16:04 +0800

    Implement barbarian invasion

--- src/collection/standard/BarbarianInvasion.ts
+++ src/collection/standard/BarbarianInvasion.ts
@@ -1,10 +1,43 @@
 import { CardSuit as Suit } from '@karuta/sanguosha-core';
 
-import TrickCard from '../TrickCard';
+import AreaEffectTrickCard from './AreaEffectTrickCard';
+import CardAction from '../../core/CardAction';
+import CardPattern from '../../core/CardPattern';
 
-class BarbarianInvasion extends TrickCard {
+import Card from '../../driver/Card';
+import CardEffectStruct from '../../driver/CardEffectStruct';
+import CardExpense from '../../driver/CardExpense';
+import DamageStruct from '../../driver/DamageStruct';
+import GameDriver from '../../driver/GameDriver';
+
+class BarbarianInvasion extends AreaEffectTrickCard {
 	constructor(suit: Suit, number: number) {
 		super('barbarian-invasion', suit, number);
+	}
+
+	async effect(driver: GameDriver, effect: CardEffectStruct): Promise<void> {
+		const { to } = effect;
+		if (!to) {
+			return;
+		}
+
+		const cards = await to.askForCards(to.getHandArea(), {
+			action: CardAction.Expend,
+			minNum: 0,
+			maxNum: 1,
+			pattern: new CardPattern({ namePostfix: 'strike' }),
+		});
+
+		if (cards.length <= 0) {
+			const damage = new DamageStruct(effect.from, to, 1);
+			damage.card = this;
+			await driver.damage(damage);
+		} else {
+			const card = cards[0] as Card;
+			const expense = new CardExpense(to, card);
+			expense.origin = effect;
+			await driver.expendCard(expense);
+		}
 	}
 }
 
