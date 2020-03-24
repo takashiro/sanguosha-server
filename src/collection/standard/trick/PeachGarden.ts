8fa33412c7cb302134c3b8b62a1d098a6776cc30
Kazuichi Takashiro <takashiro@qq.com>
2020-03-24 21:31:15 +0800

    Implement PeachGarden

--- src/collection/standard/trick/PeachGarden.ts
+++ src/collection/standard/trick/PeachGarden.ts
@@ -1,10 +1,24 @@
 import { CardSuit as Suit } from '@karuta/sanguosha-core';
 
 import GlobalEffectTrickCard from '../../GlobalEffectTrickCard';
+import GameDriver from '../../../driver';
+import CardEffect from '../../../driver/CardEffect';
+import Recover from '../../../driver/Recover';
 
 class PeachGarden extends GlobalEffectTrickCard {
 	constructor(suit: Suit, number: number) {
 		super('peach-garden', suit, number);
+	}
+
+	async effect(driver: GameDriver, effect: CardEffect): Promise<void> {
+		const { to } = effect;
+		if (!to || !to.isDead()) {
+			return;
+		}
+
+		const recover = new Recover(effect.from, to, 1);
+		recover.card = this;
+		await driver.recover(recover);
 	}
 }
 
