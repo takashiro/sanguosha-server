547c43f8275ff221a13c00e59ff4472754556dc2
Kazuichi Takashiro <takashiro@qq.com>
2020-04-13 21:44:36 +0800

    Implement Indulgence

--- src/collection/standard/trick/Indulgence.ts
+++ src/collection/standard/trick/Indulgence.ts
@@ -1,10 +1,39 @@
-import { CardSuit as Suit } from '@karuta/sanguosha-core';
+import {
+	CardSuit as Suit,
+	PlayerPhase as Phase,
+} from '@karuta/sanguosha-core';
 
 import DelayedTrickCard from '../../DelayedTrickCard';
+import GameDriver from '../../../driver/GameDriver';
+import CardEffect from '../../../driver/CardEffect';
+import Judgement from '../../../driver/Judgement';
+import CardPattern from '../../../core/CardPattern';
+
+const effectivePattern = new CardPattern({
+	suits: [
+		Suit.Spade,
+		Suit.Club,
+		Suit.Diamond,
+	],
+});
 
 class Indulgence extends DelayedTrickCard {
 	constructor(suit: Suit, number: number) {
 		super('indulgence', suit, number);
+	}
+
+	async effect(driver: GameDriver, effect: CardEffect): Promise<void> {
+		const { to } = effect;
+		if (!to) {
+			return;
+		}
+
+		const judgement = new Judgement(to, this, effectivePattern);
+		await driver.judge(judgement);
+
+		if (judgement.effective) {
+			to.skipPhase(Phase.Play);
+		}
 	}
 }
 
