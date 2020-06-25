d228e97f42bc3e35b6064d795d80252575884781
Kazuichi Takashiro <takashiro@qq.com>
2020-06-25 19:49:07 +0800

    Implement skill MaShu

--- src/collection/standard/skill/MaShu.ts
+++ src/collection/standard/skill/MaShu.ts
@@ -1,10 +1,23 @@
 import {
-	Skill,
 	SkillTag as Tag,
+	SkillOwner as Owner,
 } from '@karuta/sanguosha-core';
 
-export default class MaShu extends Skill {
-	constructor() {
-		super('mashu', [Tag.Compulsory]);
+import MonadicSkill from '../../../base/MonadicSkill';
+import DistanceVector from '../../../driver/DistanceVector';
+import GameEvent from '../../../driver/GameEvent';
+
+export default class MaShu extends MonadicSkill<DistanceVector> {
+	constructor(owner: Owner) {
+		super(owner, 'mashu', GameEvent.CalculatingDistance, [Tag.Compulsory]);
+	}
+
+	isTriggerable(vector: DistanceVector): boolean {
+		return vector.from === this.getOwner() && vector.to !== this.getOwner();
+	}
+
+	async process(vector: DistanceVector): Promise<boolean> {
+		vector.distance -= 1;
+		return false;
 	}
 }
