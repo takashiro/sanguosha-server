b7956b26fb84dd367db7a93b3b1e84c765ddb484
Kazuichi Takashiro <takashiro@qq.com>
2020-07-01 22:43:53 +0800

    Implement BiYue

--- src/collection/standard/skill/BiYue.ts
+++ src/collection/standard/skill/BiYue.ts
@@ -1,7 +1,25 @@
-import { Skill, SkillOwner } from '@karuta/sanguosha-core';
+import {
+	SkillOwner,
+	PlayerPhase as Phase,
+} from '@karuta/sanguosha-core';
 
-export default class BiYue extends Skill {
+import MonadicSkill from '../../../base/MonadicSkill';
+import GameEvent from '../../../driver/GameEvent';
+import PhaseChange from '../../../driver/PhaseChange';
+
+export default class BiYue extends MonadicSkill<PhaseChange> {
 	constructor(owner: SkillOwner) {
-		super(owner, 'biyue');
+		super(owner, 'biyue', GameEvent.ProceedingPhase);
+	}
+
+	isTriggerable(change: PhaseChange): boolean {
+		return change.to === Phase.End && change.player === this.getOwner();
+	}
+
+	async process(change: PhaseChange): Promise<boolean> {
+		const { player } = change;
+		const driver = this.getDriver();
+		await driver?.drawCards(player, 1);
+		return false;
 	}
 }
