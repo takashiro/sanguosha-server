e2e5882898c09951f7772e18529e744a31a2c43a
Kazuichi Takashiro <takashiro@qq.com>
2020-07-02 22:06:01 +0800

    Implement YingZi

--- src/collection/standard/skill/YingZi.ts
+++ src/collection/standard/skill/YingZi.ts
@@ -1,7 +1,24 @@
-import { Skill, SkillOwner } from '@karuta/sanguosha-core';
+import {
+	SkillOwner,
+	PlayerPhase as Phase,
+} from '@karuta/sanguosha-core';
 
-export default class YingZi extends Skill {
+import CardDraw from '../../../driver/CardDraw';
+import GameEvent from '../../../driver/GameEvent';
+import MonadicSkill from '../../../base/MonadicSkill';
+
+export default class YingZi extends MonadicSkill<CardDraw> {
 	constructor(owner: SkillOwner) {
-		super(owner, 'yingzi');
+		super(owner, 'yingzi', GameEvent.DrawingNCards);
+	}
+
+	isTriggerable(draw: CardDraw): boolean {
+		const { player } = draw;
+		return player === this.getOwner() && player.getPhase() === Phase.Draw;
+	}
+
+	async process(draw: CardDraw): Promise<boolean> {
+		draw.num++;
+		return false;
 	}
 }
