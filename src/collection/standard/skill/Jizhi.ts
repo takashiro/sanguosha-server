881e5321ea2a230069ec1caf1041cebc4830b01b
Kazuichi Takashiro <takashiro@qq.com>
2020-07-04 09:31:55 +0800

    Implement JiZhi

--- src/collection/standard/skill/Jizhi.ts
+++ src/collection/standard/skill/Jizhi.ts
@@ -1,7 +1,26 @@
-import { Skill, SkillOwner } from '@karuta/sanguosha-core';
+import {
+	SkillOwner,
+	CardType,
+	CardSubtype,
+} from '@karuta/sanguosha-core';
 
-export default class JiZhi extends Skill {
+import MonadicSkill from '../../../base/MonadicSkill';
+import CardUse from '../../../driver/CardUse';
+import GameEvent from '../../../driver/GameEvent';
+
+export default class JiZhi extends MonadicSkill<CardUse> {
 	constructor(owner: SkillOwner) {
-		super(owner, 'jizhi');
+		super(owner, 'jizhi', GameEvent.UsingCard);
+	}
+
+	isTriggerable(use: CardUse): boolean {
+		return use.from === this.getOwner() && use.card.getType() === CardType.Trick && use.card.getSubtype() === CardSubtype.InstantTrick;
+	}
+
+	async process(use: CardUse): Promise<boolean> {
+		const { from } = use;
+		const driver = this.getDriver();
+		await driver?.drawCards(from, 1);
+		return false;
 	}
 }
