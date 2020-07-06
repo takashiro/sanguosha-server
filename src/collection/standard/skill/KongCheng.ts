f07f7e0c0f2763bb86da497cf23b6d77de1b62e3
Kazuichi Takashiro <takashiro@qq.com>
2020-07-06 22:26:09 +0800

    Implement KongCheng

--- src/collection/standard/skill/KongCheng.ts
+++ src/collection/standard/skill/KongCheng.ts
@@ -1,11 +1,42 @@
 import {
-	Skill,
 	SkillTag as Tag,
 	SkillOwner,
 } from '@karuta/sanguosha-core';
 
-export default class KongCheng extends Skill {
+import CardUse from '../../../driver/CardUse';
+import GameEvent from '../../../driver/GameEvent';
+import MonadicSkill from '../../../base/MonadicSkill';
+
+export default class KongCheng extends MonadicSkill<CardUse> {
 	constructor(owner: SkillOwner) {
-		super(owner, 'kongcheng', [Tag.Compulsory]);
+		super(owner, 'kongcheng', GameEvent.BeingCardTargets, [Tag.Compulsory]);
+	}
+
+	isTriggerable(use: CardUse): boolean {
+		const cardName = use.card.getName();
+		if (cardName !== 'duel' && !cardName.endsWith('strike')) {
+			return false;
+		}
+
+		const owner = this.getOwner();
+		for (const to of use.to) {
+			if (to !== owner) {
+				continue;
+			}
+			const handArea = to.getHandArea();
+			return handArea.size <= 0;
+		}
+		return false;
+	}
+
+	async process(use: CardUse): Promise<boolean> {
+		const owner = this.getOwner();
+		for (let i = 0; i < use.to.length; i++) {
+			if (use.to[i] === owner) {
+				use.to.splice(i, 1);
+				break;
+			}
+		}
+		return false;
 	}
 }
