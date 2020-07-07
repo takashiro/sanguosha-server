b311d94f47dc4dc08a45dccfa16dbc9504342673
Kazuichi Takashiro <takashiro@qq.com>
2020-07-07 22:10:29 +0800

    Implement JianXiong

--- src/collection/standard/skill/JianXiong.ts
+++ src/collection/standard/skill/JianXiong.ts
@@ -1,7 +1,36 @@
-import { Skill, SkillOwner } from '@karuta/sanguosha-core';
+import {
+	SkillOwner,
+	CardAreaType,
+} from '@karuta/sanguosha-core';
 
-export default class JianXiong extends Skill {
+import Card from '../../../driver/Card';
+import Damage from '../../../driver/Damage';
+import GameEvent from '../../../driver/GameEvent';
+import MonadicSkill from '../../../base/MonadicSkill';
+
+export default class JianXiong extends MonadicSkill<Damage> {
 	constructor(owner: SkillOwner) {
-		super(owner, 'jianxiong');
+		super(owner, 'jianxiong', GameEvent.AfterDamaged);
+	}
+
+	isTriggerable(damage: Damage): boolean {
+		return Boolean(damage.card) && damage.to === this.getOwner();
+	}
+
+	async process(damage: Damage): Promise<boolean> {
+		if (!damage.card) {
+			return false;
+		}
+
+		const card = damage.card as Card;
+		const location = card.getLocation();
+		if (!location || location.getType() !== CardAreaType.Process) {
+			return false;
+		}
+
+		const driver = this.getDriver();
+		await driver?.moveCards([card], damage.to.getHandArea(), { open: true });
+
+		return false;
 	}
 }
