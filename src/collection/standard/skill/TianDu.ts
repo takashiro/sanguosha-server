713881e9d54f5dbcd06014f33505fde8d0762c0f
Kazuichi Takashiro <takashiro@qq.com>
2020-07-08 22:15:20 +0800

    Implement TianDu

--- src/collection/standard/skill/TianDu.ts
+++ src/collection/standard/skill/TianDu.ts
@@ -1,7 +1,32 @@
-import { Skill, SkillOwner } from '@karuta/sanguosha-core';
+import { SkillOwner } from '@karuta/sanguosha-core';
 
-export default class TianDu extends Skill {
+import GameEvent from '../../../driver/GameEvent';
+import Judgement from '../../../driver/Judgement';
+import MonadicSkill from '../../../base/MonadicSkill';
+
+export default class TianDu extends MonadicSkill<Judgement> {
 	constructor(owner: SkillOwner) {
-		super(owner, 'tiandu');
+		super(owner, 'tiandu', GameEvent.AfterIssuingJudgement);
+	}
+
+	isTriggerable(judgement: Judgement): boolean {
+		const { player } = judgement;
+		return player === this.getOwner();
+	}
+
+	async process(judgement: Judgement): Promise<boolean> {
+		const driver = this.getDriver();
+		if (!driver) {
+			return false;
+		}
+
+		const card = judgement.getCard();
+		if (card) {
+			const { player } = judgement;
+			await driver.moveCards([card], player.getHandArea(), { open: true });
+			return true;
+		}
+
+		return false;
 	}
 }
