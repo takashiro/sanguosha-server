bc28f9abf3357eca9e2a7e323cc0f31e31c17dfd
Kazuichi Takashiro <takashiro@qq.com>
2021-02-09 16:52:25 +0800

    Move quick classes to another package

--- src/base/HorseCard.ts
+++
@@ -1,6 +0,0 @@
-import EquipCard from './EquipCard';
-
-abstract class HorseCard extends EquipCard {
-}
-
-export default HorseCard;
--- src/base/Skill.ts
+++
@@ -1,8 +0,0 @@
-import { Skill as MetaSkill } from '@karuta/sanguosha-core';
-
-import SkillEffect from './SkillEffect';
-
-export default abstract class Skill extends MetaSkill {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	abstract getEffects(): SkillEffect<any>[];
-}
