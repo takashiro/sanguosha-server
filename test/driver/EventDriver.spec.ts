a8b05093bda150295427668672d04b6cd73c3bed
Kazuichi Takashiro <takashiro@qq.com>
2021-10-06 16:22:50 +0800

    Test EventDriver in *.ts

--- test/driver/EventDriver.spec.ts
+++ test/driver/EventDriver.spec.ts
@@ -1,20 +1,15 @@
 import { EventListener } from '@karuta/sanguosha-pack';
 import EventDriver from '../../src/driver/EventDriver';
 
-class FakeTrigger extends EventListener {
-	select() {
+class FakeTrigger extends EventListener<number> {
+	async select() {
 		return 0;
 	}
 }
 
 it('should refuse undefined event', () => {
-	expect.assertions(1);
 	const driver = new EventDriver();
-	try {
-		driver.register({});
-	} catch (error) {
-		expect(error.message).toBeTruthy();
-	}
+	expect(() => driver.register({} as unknown as EventListener<unknown>)).toThrowError('Failed to register undefined event handler');
 });
 
 it('triggers corresponding listeners', async () => {
@@ -40,7 +35,7 @@ it('triggers corresponding listeners', async () => {
 });
 
 it('unregisters a listener', async () => {
-	const driver = new EventDriver(1);
+	const driver = new EventDriver();
 	const trigger = new FakeTrigger(1);
 	const process = jest.spyOn(trigger, 'process');
 
@@ -50,29 +45,25 @@ it('unregisters a listener', async () => {
 
 	process.mockClear();
 	driver.unregister(trigger);
-	await driver.trigger(1);
+	await driver.trigger(1, undefined);
 	expect(process).not.toBeCalled();
 });
 
 it('unregisters an invalid listener', async () => {
 	const driver = new EventDriver();
-	expect.assertions(1);
-	try {
-		driver.unregister({});
-	} catch (error) {
-		expect(error.message).toBeTruthy();
-	}
+	expect(() => driver.unregister({} as unknown as EventListener<unknown>)).toThrowError('Failed to unregister undefined event handler');
 });
 
 it('unregisters a non-existing listener', async () => {
 	const driver = new EventDriver();
-	driver.unregister({ event: 1 });
-	expect(driver.listeners.size).toBe(0);
+	const listeners = Reflect.get(driver, 'listeners') as Map<number, EventListener<unknown>[]>;
+	driver.unregister({ event: 1 } as unknown as EventListener<unknown>);
+	expect(listeners.size).toBe(0);
 
 	driver.register(new FakeTrigger(1));
-	driver.unregister({ event: 1 });
-	expect(driver.listeners.size).toBe(1);
-	expect(driver.listeners.get(1).length).toBe(1);
+	driver.unregister({ event: 1 } as unknown as EventListener<unknown>);
+	expect(listeners.size).toBe(1);
+	expect(listeners?.get(1)?.length).toBe(1);
 });
 
 it('triggers nothing if it is stopped', async () => {
@@ -83,7 +74,7 @@ it('triggers nothing if it is stopped', async () => {
 	driver.register(trigger);
 	driver.stop();
 	const isStopped = jest.spyOn(driver, 'isStopped');
-	await driver.trigger(1);
+	await driver.trigger(1, undefined);
 	expect(isStopped).toBeCalledTimes(1);
 	expect(process).not.toBeCalled();
 });
@@ -95,7 +86,7 @@ it('triggers nothing if the listener is not selected', async () => {
 	const process = jest.spyOn(trigger, 'process');
 	driver.register(trigger);
 
-	select.mockReturnValue(-1);
+	select.mockResolvedValue(-1);
 	await driver.trigger(1, {});
 
 	expect(process).not.toBeCalled();
@@ -103,11 +94,11 @@ it('triggers nothing if the listener is not selected', async () => {
 
 it('always trigger compulsory listeners', async () => {
 	const t1 = new FakeTrigger(1);
-	jest.spyOn(t1, 'select').mockReturnValue(-1);
+	jest.spyOn(t1, 'select').mockResolvedValue(-1);
 
 	const t2 = new FakeTrigger(1);
 	jest.spyOn(t2, 'isCompulsory').mockReturnValue(true);
-	jest.spyOn(t2, 'select').mockReturnValue(-1);
+	jest.spyOn(t2, 'select').mockResolvedValue(-1);
 	const process = jest.spyOn(t2, 'process');
 
 	const driver = new EventDriver();
