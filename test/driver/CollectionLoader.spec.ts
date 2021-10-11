23b0b1a7732caed69706301149b7019a5389389a
Kazuichi Takashiro <takashiro@qq.com>
2021-10-11 21:06:07 +0800

    Test normal cases of CollectionLoader

--- test/driver/CollectionLoader.spec.ts
+++ test/driver/CollectionLoader.spec.ts
@@ -1,4 +1,34 @@
+import fs from 'fs';
+
 import CollectionLoader from '../../src/driver/CollectionLoader';
+
+describe('Normal Case', () => {
+	let readFile: jest.SpyInstance<Promise<string | Buffer>>;
+
+	beforeAll(() => {
+		readFile = jest.spyOn(fs.promises, 'readFile').mockResolvedValueOnce(JSON.stringify({
+			dependencies: {
+				'@karuta/sanguosha-pack': '^1.0.0',
+			},
+		}));
+	});
+
+	const loader = new CollectionLoader();
+
+	it('loads a valid module', async () => {
+		const col = await loader.get('jest');
+		expect(col).toBeTruthy();
+	});
+
+	it('caches loaded modules', async () => {
+		const col = await loader.get('jest');
+		expect(col).toBeTruthy();
+	});
+
+	afterAll(() => {
+		readFile.mockRestore();
+	});
+});
 
 describe('Error Handling', () => {
 	const loader = new CollectionLoader();
